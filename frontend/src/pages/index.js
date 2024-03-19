5252b5731d39cd3d8ac72b942e9b0917ca72add4
Xiu <xiuying623@gmail.com>
2024-03-19 00:22:22 -0400

    first commit

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <main>
+      <Layout>
+        <h1>Welcome to My Next.js Site</h1>
+        {/* 其他内容 */}
+      </Layout>
+
+    </main>
+  );
+}
---
+++ frontend/src/pages/students/index.jsx
@@ -0,0 +1,3 @@
+export default function students (){
+    return (null)
+}
