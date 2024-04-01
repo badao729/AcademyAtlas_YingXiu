5703c672f580c1ef04586f51a04c368bd2a52fd1
Xiu <xiuying623@gmail.com>
2024-04-01 16:30:08 -0400

    add about part

--- frontend/src/app/(contact)/about/page.js
+++ frontend/src/app/(contact)/about/page.js
@@ -13,9 +13,15 @@ const About = () => {
             <Nav />
             <div className="about-body">
                 <div className="ying">
-                    <img className="ying-img anima-scaleUp"
+                    <div><img className="ying-img anima-scaleUp"
                         src="/photo/Ying.jpeg"
-                        alt="logo" />
+                        alt="logo" /></div>
+
+                    <div className="intro">
+                        <h2>Ying Xiu</h2>
+                        <h3>Financial Analyst | Software Development | Graphic Design</h3>
+                        <h4>As a Software Engineer with a distinctive blend of financial insight and design expertise.</h4>
+                    </div>
                 </div>
 
                 <div className="shiba">
